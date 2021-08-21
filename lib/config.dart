4ec484d7c52c651f9fadb362bb0a901845759cc0
saif19196 <saifullah19196@gmail.com>
2021-08-21 10:33:18 +0530

    grocery app

---
+++ lib/config.dart
@@ -0,0 +1,6 @@
+class Config{
+  static String key ="ck_262afd2687f8fce4410d50a2892ea2f8edda54ce";
+  static String sceret="cs_edc35911431856807030730e977dab82c6cbcb35";
+  static String url ="https://azadessentials.com/wp-json/wc/v3/";
+  static String customerUrl ="customers";
+}
